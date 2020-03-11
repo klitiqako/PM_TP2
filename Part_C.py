718aa7550caaedca04e72645f5d73a5b2409c2c0
gabrielshieldsg <59936544+gabrielshieldsg@users.noreply.github.com>
2020-03-10 23:00:25 -0400

    Partie_C 1)

--- Part_C.py
+++ Part_C.py
@@ -36,11 +36,22 @@ industries = df_Returns.columns
 
 
 ##Q1:
-Market_cap_PartC = df_Firm_Size*df_NB_Firms
-Momentum = df_Returns.rolling(12).mean()
-Book_to_Mkt = np.ones((len(df_Returns),1))
-Book_to_Mkt= pd.DataFrame(Book_to_Mkt)
-Book_to_Mkt.index = df_Returns.index
-for i in range(0,len(Book_to_Mkt)):
-    if 
-    
+Market_cap_C = df_Firm_Size*df_NB_Firms
+Momentum_C = df_Returns.rolling(12).mean()
+Momentum_C = pd.DataFrame.dropna(Momentum_C)
+Book_to_Mkt_C = np.ones((len(df_Returns),len(industries)))
+Book_to_Mkt_C= pd.DataFrame(Book_to_Mkt_C)
+Book_to_Mkt_C.index = df_Returns.index
+Book_to_Mkt_C.columns = industries
+for i in range(0,len(Book_to_Mkt_C)):
+    for j in range(0,len(industries)):
+        if Book_to_Mkt_C.index[i].year == df_MktBook.index[0].year:
+            B_M_1 = df_MktBook[df_MktBook.index.year == Book_to_Mkt_C.index[i].year].iloc[0,j]
+            Book_to_Mkt_C.iloc[i,j] = B_M_1
+        elif Book_to_Mkt_C.index[i].month <= 6:
+            B_M_2 = df_MktBook[df_MktBook.index.year == Book_to_Mkt_C.index[i].year-1].iloc[0,j]
+            Book_to_Mkt_C.iloc[i,j] = B_M_2
+        else:
+            B_M_3 = df_MktBook[df_MktBook.index.year == Book_to_Mkt_C.index[i].year].iloc[0,j]
+            Book_to_Mkt_C.iloc[i,j] = B_M_3
+
