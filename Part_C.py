e67755cce39830453b5aa655c5d2ad77bf64bc99
gabrielshieldsg <59936544+gabrielshieldsg@users.noreply.github.com>
2020-03-17 09:29:20 -0400

    Working on Q4

--- Part_C.py
+++ Part_C.py
@@ -76,13 +76,14 @@ for i in range(0,len(Book_to_Mkt_C)):
 ###############Q2
 Months = df_Returns.index
 Months = Months.insert(1123,pd.to_datetime('20200201', format= '%Y%m%d'))
-betas = np.ones((len(Months)-12,len(industries)))
+betas = np.ones((len(df_Returns)-11,len(industries)))
 betas= pd.DataFrame(betas)
-betas.index = Months[12:]
+betas.index = df_Returns.index[11:]
 betas.columns = industries
+df_Daily_Returns[df_Daily_Returns == -99.99] = np.nan
 
 
-for i in range(0,len(Months)-12): 
+for i in range(0,len(df_Returns)-11): 
     for j in range(0,len(industries)):
         d = Months[12+i]
         d_Last12 = d - dateutil.relativedelta.relativedelta(months=11)
@@ -104,15 +105,15 @@ time_elapsed2 = (time.clock() - time_start)
 ##Analytical takes half as long
 
 ###############Q3
-Idio_vol = np.ones((len(Months),len(industries)))
-Idio_vol= pd.DataFrame(Idio_vol[1:])
-Idio_vol.index = Months
+Idio_vol = np.ones((len(df_Returns),len(industries)))
+Idio_vol= pd.DataFrame(Idio_vol)
+Idio_vol.index = df_Returns.index
 Idio_vol.columns = industries
 
 
 for i in range(0,len(Idio_vol)): 
     for j in range(0,len(industries)):
-        d = Idio_vol.index[i]
+        d = Months[i+1]
         d_Last12 = d - dateutil.relativedelta.relativedelta(months=1)
         X = df_Fama.iloc[:,:3][(df_Fama.index < d)&(df_Fama.index >= d_Last12)]
         X = sm.add_constant(X)
@@ -120,3 +121,180 @@ for i in range(0,len(Idio_vol)):
         est = sm.OLS(Y,X).fit()
         std_resid = np.std(est.resid)
         Idio_vol.iloc[i,j] = std_resid
+
+###############Q4
+Volatility_Q4 = Idio_vol[11:]
+Book_to_Mkt_Q4 = Book_to_Mkt_C[11:]
+Market_cap_Q4 = Market_cap_C[11:]
+
+Book_to_Mkt_Q4[Book_to_Mkt_Q4 == -99.99] = np.nan
+Market_cap_Q4[Market_cap_Q4 == -0] = np.nan
+Momentum_C[Momentum_C == -99.99] = np.nan
+
+
+
+Rank_Vol = np.ones((len(betas),len(industries)))
+Rank_Book_Mkt = np.ones((len(betas),len(industries)))
+Rank_Mkt_cap = np.ones((len(betas),len(industries)))
+Rank_Momemtum = np.ones((len(betas),len(industries)))
+Rank_Betas = np.ones((len(betas),len(industries)))
+
+Rank_Vol= pd.DataFrame(Rank_Vol)
+Rank_Book_Mkt= pd.DataFrame(Rank_Book_Mkt)
+Rank_Mkt_cap= pd.DataFrame(Rank_Mkt_cap)
+Rank_Momemtum= pd.DataFrame(Rank_Momemtum)
+Rank_Betas= pd.DataFrame(Rank_Betas)
+
+Rank_Vol.columns = industries
+Rank_Book_Mkt.columns = industries
+Rank_Mkt_cap.columns = industries
+Rank_Momemtum.columns = industries
+Rank_Betas.columns = industries
+
+Rank_Vol.index = betas.index
+Rank_Book_Mkt.index = betas.index
+Rank_Mkt_cap.index = betas.index
+Rank_Momemtum.index = betas.index
+Rank_Betas.index = betas.index
+
+
+for i in range(0,len(betas)):
+    rank1 = Volatility_Q4.iloc[i,:].rank()
+    Rank_Vol.iloc[i,:] = rank1
+    rank2 = Book_to_Mkt_Q4.iloc[i,:].rank()
+    Rank_Book_Mkt.iloc[i,:] = rank2
+    rank3 = Market_cap_Q4.iloc[i,:].rank()
+    Rank_Mkt_cap.iloc[i,:] = rank3
+    rank4 = Momentum_C.iloc[i,:].rank()
+    Rank_Momemtum.iloc[i,:] = rank4
+    rank5 = betas.iloc[i,:].rank()
+    Rank_Betas.iloc[i,:] = rank5
+
+
+#a = np.where(Rank_Betas>30,0.2,(np.where(Rank_Betas<=5,-0.2,0)))
+
+
+#Equal-weighted
+Weight_Vol = np.ones((len(betas),len(industries)))
+Weight_Book_Mkt = np.ones((len(betas),len(industries)))
+Weight_Mkt_cap = np.ones((len(betas),len(industries)))
+Weight_Momemtum = np.ones((len(betas),len(industries)))
+Weight_Betas = np.ones((len(betas),len(industries)))
+
+Weight_Vol= pd.DataFrame(Weight_Vol)
+Weight_Book_Mkt= pd.DataFrame(Weight_Book_Mkt)
+Weight_Mkt_cap= pd.DataFrame(Weight_Mkt_cap)
+Weight_Momemtum= pd.DataFrame(Weight_Momemtum)
+Weight_Betas= pd.DataFrame(Weight_Betas)
+
+Weight_Vol.columns = industries
+Weight_Book_Mkt.columns = industries
+Weight_Mkt_cap.columns = industries
+Weight_Momemtum.columns = industries
+Weight_Betas.columns = industries
+
+Weight_Vol.index = betas.index
+Weight_Book_Mkt.index = betas.index
+Weight_Mkt_cap.index = betas.index
+Weight_Momemtum.index = betas.index
+Weight_Betas.index = betas.index
+
+for i in range(0,len(betas)):
+    max1 = min(Rank_Vol.iloc[i,:].nlargest(5))
+    min1 = max(Rank_Vol.iloc[i,:].nsmallest(5))
+    Weight_Vol.iloc[i,:] = np.where(Rank_Vol.iloc[i,:]>=max1,0.2,(np.where(Rank_Vol.iloc[i,:]<=min1,-0.2,0)))
+    max2 = min(Rank_Book_Mkt.iloc[i,:].nlargest(5))
+    min2 = max(Rank_Book_Mkt.iloc[i,:].nsmallest(5))
+    Weight_Book_Mkt.iloc[i,:] = np.where(Rank_Book_Mkt.iloc[i,:]>=max2,0.2,(np.where(Rank_Book_Mkt.iloc[i,:]<=min2,-0.2,0)))
+    max3 = min(Rank_Mkt_cap.iloc[i,:].nlargest(5))
+    min3 = max(Rank_Mkt_cap.iloc[i,:].nsmallest(5))
+    Weight_Mkt_cap.iloc[i,:] = np.where(Rank_Mkt_cap.iloc[i,:]>=max3,0.2,(np.where(Rank_Mkt_cap.iloc[i,:]<=min3,-0.2,0)))
+    max4 = min(Rank_Momemtum.iloc[i,:].nlargest(5))
+    min4 = max(Rank_Momemtum.iloc[i,:].nsmallest(5))
+    Weight_Momemtum.iloc[i,:] = np.where(Rank_Momemtum.iloc[i,:]>=max4,0.2,(np.where(Rank_Momemtum.iloc[i,:]<=min4,-0.2,0)))
+    max5 = min(Rank_Betas.iloc[i,:].nlargest(5))
+    min5 = max(Rank_Betas.iloc[i,:].nsmallest(5))
+    Weight_Betas.iloc[i,:] = np.where(Rank_Betas.iloc[i,:]>=max5,0.2,(np.where(Rank_Betas.iloc[i,:]<=min5,-0.2,0)))
+    
+#Market_Cap-weighted
+Weight_VW_Vol = np.ones((len(betas),len(industries)))
+Weight_VW_Book_Mkt = np.ones((len(betas),len(industries)))
+Weight_VW_Mkt_cap = np.ones((len(betas),len(industries)))
+Weight_VW_Momemtum = np.ones((len(betas),len(industries)))
+Weight_VW_Betas = np.ones((len(betas),len(industries)))
+
+Weight_VW_Vol= pd.DataFrame(Weight_VW_Vol)
+Weight_VW_Book_Mkt= pd.DataFrame(Weight_VW_Book_Mkt)
+Weight_VW_Mkt_cap= pd.DataFrame(Weight_VW_Mkt_cap)
+Weight_VW_Momemtum= pd.DataFrame(Weight_VW_Momemtum)
+Weight_VW_Betas= pd.DataFrame(Weight_VW_Betas)
+
+Weight_VW_Vol.columns = industries
+Weight_VW_Book_Mkt.columns = industries
+Weight_VW_Mkt_cap.columns = industries
+Weight_VW_Momemtum.columns = industries
+Weight_VW_Betas.columns = industries
+
+Weight_VW_Vol.index = betas.index
+Weight_VW_Book_Mkt.index = betas.index
+Weight_VW_Mkt_cap.index = betas.index
+Weight_VW_Momemtum.index = betas.index
+Weight_VW_Betas.index = betas.index
+
+for i in range(0,len(betas)):
+    large1 = Rank_Vol.iloc[i,:].nlargest(5)
+    Market_Cap_L1 = sum(Market_cap_Q4.loc[betas.index[i],(large1.index)])
+    small1 = Rank_Vol.iloc[i,:].nsmallest(5)
+    Market_Cap_S1 = sum(Market_cap_Q4.loc[betas.index[i],(small1.index)])
+    max1 = min(large1)
+    min1 = max(small1)
+    
+    large2 = Rank_Book_Mkt.iloc[i,:].nlargest(5)
+    Market_Cap_L2 = sum(Market_cap_Q4.loc[betas.index[i],(large2.index)])
+    small2 = Rank_Book_Mkt.iloc[i,:].nsmallest(5)
+    Market_Cap_S2 = sum(Market_cap_Q4.loc[betas.index[i],(small2.index)])
+    max2 = min(large2)
+    min2 = max(small2)
+    
+    large3 = Rank_Mkt_cap.iloc[i,:].nlargest(5)
+    Market_Cap_L3 = sum(Market_cap_Q4.loc[betas.index[i],(large3.index)])
+    small3 = Rank_Mkt_cap.iloc[i,:].nsmallest(5)
+    Market_Cap_S3 = sum(Market_cap_Q4.loc[betas.index[i],(small3.index)])
+    max3 = min(large3)
+    min3 = max(small3)
+    
+    large4 = Rank_Momemtum.iloc[i,:].nlargest(5)
+    Market_Cap_L4 = sum(Market_cap_Q4.loc[betas.index[i],(large4.index)])
+    small4 = Rank_Momemtum.iloc[i,:].nsmallest(5)
+    Market_Cap_S4 = sum(Market_cap_Q4.loc[betas.index[i],(small4.index)])
+    max4 = min(large4)
+    min4 = max(small4)
+    
+    large5 = Rank_Betas.iloc[i,:].nlargest(5)
+    Market_Cap_L5 = sum(Market_cap_Q4.loc[betas.index[i],(large5.index)])
+    small5 = Rank_Betas.iloc[i,:].nsmallest(5)
+    Market_Cap_S5 = sum(Market_cap_Q4.loc[betas.index[i],(small5.index)])
+    max5 = min(large5)
+    min5 = max(small5)
+    for j in range(0,5):
+        Weight_VW_Vol.loc[betas.index[i],large1.index[j]] = round(Market_cap_Q4.loc[betas.index[i],(large1.index[j])]/Market_Cap_L1,3)
+        Weight_VW_Vol.loc[betas.index[i],small1.index[j]] = -round(Market_cap_Q4.loc[betas.index[i],(small1.index[j])]/Market_Cap_S1,3)
+        
+        Weight_VW_Book_Mkt.loc[betas.index[i],large2.index[j]] = round(Market_cap_Q4.loc[betas.index[i],(large2.index[j])]/Market_Cap_L2,3)
+        Weight_VW_Book_Mkt.loc[betas.index[i],small2.index[j]] = -round(Market_cap_Q4.loc[betas.index[i],(small2.index[j])]/Market_Cap_S2,3)
+        
+        Weight_VW_Mkt_cap.loc[betas.index[i],large3.index[j]] = round(Market_cap_Q4.loc[betas.index[i],(large3.index[j])]/Market_Cap_L3,3)
+        Weight_VW_Mkt_cap.loc[betas.index[i],small3.index[j]] = -round(Market_cap_Q4.loc[betas.index[i],(small3.index[j])]/Market_Cap_S3,3)
+        
+        Weight_VW_Momemtum.loc[betas.index[i],large4.index[j]] = round(Market_cap_Q4.loc[betas.index[i],(large4.index[j])]/Market_Cap_L4,3)
+        Weight_VW_Momemtum.loc[betas.index[i],small4.index[j]] = -round(Market_cap_Q4.loc[betas.index[i],(small4.index[j])]/Market_Cap_S4,3)
+        
+        Weight_VW_Betas.loc[betas.index[i],large5.index[j]] = round(Market_cap_Q4.loc[betas.index[i],(large5.index[j])]/Market_Cap_L5,3)
+        Weight_VW_Betas.loc[betas.index[i],small5.index[j]] = -round(Market_cap_Q4.loc[betas.index[i],(small5.index[j])]/Market_Cap_S5,3)
+   
+    Weight_VW_Vol.iloc[i,:][Weight_VW_Vol.iloc[i,:]==1] = 0
+    Weight_VW_Book_Mkt.iloc[i,:][Weight_VW_Book_Mkt.iloc[i,:]==1] = 0
+    Weight_VW_Mkt_cap.iloc[i,:][Weight_VW_Mkt_cap.iloc[i,:]==1] = 0
+    Weight_VW_Momemtum.iloc[i,:][Weight_VW_Momemtum.iloc[i,:]==1] = 0
+    Weight_VW_Betas.iloc[i,:][Weight_VW_Betas.iloc[i,:]==1] = 0
+
