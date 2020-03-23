aaf73ef3fd0b95650e3edfbbcbac79adc911c723
klitiqako <59936626+klitiqako@users.noreply.github.com>
2020-03-22 23:28:13 -0400

    debugged prtf 1-6

--- TP2_main.py
+++ TP2_main.py
@@ -71,7 +71,7 @@ for date in date_vec_btst:
 
     working_risk_free_rates = all_risk_free_rates.loc[date_vec[idx-60]:date_vec[idx-1],['RF']]
     rf                      = working_risk_free_rates.iloc[-1]
-    rf_tplus1               = all_risk_free_rates.loc[date_vec[date_vec[idx],['RF']]
+    rf_tplus1               = all_risk_free_rates.loc[date_vec[idx],['RF']]
 
     working_avg_firm_size   = all_avg_firm_size.loc[date_vec[idx-60]:date_vec[idx-1],:]
     avg_firm_size           = working_avg_firm_size.iloc[-1]
@@ -82,58 +82,59 @@ for date in date_vec_btst:
     #1) the portfolio that maximizes the Sharpe ratio without short-sale constraints
     (tmp1, tmp2, tmp3) = myf.minvarpf(working_monthly_returns, 5, rf[0], risk_free_allowed = False, tangency = True)
     P1_weights = tmp3
-    P1_return.append(myf.prtf_return(P1_weights,montly_returns_tplus1,:]))
+    P1_return.append(myf.prtf_return(P1_weights,montly_returns_tplus1))
 
 
     #2) the portfolio that maximizes the Sharpe ratio with short-sale constraints;
     (tmp1, tmp2, tmp3) = myf.minvarpf_noshortsale(working_monthly_returns, 5, rf[0], risk_free_allowed = False, tangency = True)
     P2_weights = tmp3
-    P2_return.append(myf.prtf_return(P2_weights,montly_returns_tplus1,:]))
+    P2_return.append(myf.prtf_return(P2_weights,montly_returns_tplus1))
 
 
      #3) the portfolio where the weight of each asset is inversely related to its variance;
     P3_weights = 1/np.diag(covariance_matrix)
     Inv_variance =  sum(P3_weights)              
     P3_weights = P3_weights/Inv_variance
-    P3_return.append(myf.prtf_return(P3_weights,montly_returns_tplus1,:]))
+    P3_return.append(myf.prtf_return(P3_weights,montly_returns_tplus1))
 
 
     #4) the portfolio where the weight of each asset is inversely related to its volatility;
     P4_weights = 1/np.diag(np.sqrt(covariance_matrix))
     Inv_volatility =  sum(P4_weights)
     P4_weights = P4_weights/Inv_volatility
-    P4_return.append(myf.prtf_return(P4_weights,montly_returns_tplus1,:]))
+    P4_return.append(myf.prtf_return(P4_weights,montly_returns_tplus1))
 
 
     #5) the portfolio where assets have the same weight;
     P5_weights = np.full([n_industries],1/n_industries)
-    P5_return.append(myf.prtf_return(P5_weights,montly_returns_tplus1,:]))
+    P5_return.append(myf.prtf_return(P5_weights,montly_returns_tplus1))
 
 
     #6) the portfolio where the weight of each is linearly related to its market capitalization;
     total_market_cap = avg_firm_size @ num_firms
     P6_weights = (avg_firm_size * num_firms) / total_market_cap
     P6_weights=P6_weights.to_numpy()
-    P6_return.append(myf.prtf_return(P6_weights,montly_returns_tplus1,:]))
+    P6_return.append(myf.prtf_return(P6_weights,montly_returns_tplus1))
 
 
     #7) the portfolio with the minimum variance;
     (tmp1, tmp2, tmp3) = myf.minvarpf(working_monthly_returns,[], rf[0], risk_free_allowed = False, tangency = False)
     P7_weights = tmp3
-    P7_return.append(myf.prtf_return(P7_weights,montly_returns_tplus1,:]))
+    P7_return.append(myf.prtf_return(P7_weights,montly_returns_tplus1))
 
-NAV_P1_return = np.cumprod(1+ np.array(P1_return))
-NAV_P2_return = np.cumprod(1+ np.array(P2_return))
-NAV_P3_return = np.cumprod(1+ np.array(P3_return))
-NAV_P4_return = np.cumprod(1+ np.array(P4_return))
-NAV_P5_return = np.cumprod(1+ np.array(P5_return))
-NAV_P6_return = np.cumprod(1+ np.array(P6_return))
-NAV_P7_return = np.cumprod(1+ np.array(P7_return))
+NAV_P1_return = np.cumprod(1+ np.array(P1_return) / 100)
+NAV_P2_return = np.cumprod(1+ np.array(P2_return) / 100)
+NAV_P3_return = np.cumprod(1+ np.array(P3_return) / 100)
+NAV_P4_return = np.cumprod(1+ np.array(P4_return) / 100)
+NAV_P5_return = np.cumprod(1+ np.array(P5_return) / 100)
+NAV_P6_return = np.cumprod(1+ np.array(P6_return) / 100)
+NAV_P7_return = np.cumprod(1+ np.array(P7_return) / 100)
 
 NAV = np.transpose([NAV_P1_return, NAV_P2_return, NAV_P3_return, NAV_P4_return, NAV_P5_return, NAV_P6_return, NAV_P7_return])
 plt.plot(NAV)
-plt.yscale('log')
+#plt.yscale('log')
 plt.xlabel(date_vec_btst)
+plt.show()
 
 #P1_weight_np = np.array(P1_weights)
 #plt.plot(P1_weight_np[:10,:])
