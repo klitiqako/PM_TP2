aedfca51544f5bdb0bfd96741718a20dc245e41d
keanuvivish <55415385+keanuvivish@users.noreply.github.com>
2020-03-28 00:51:50 -0400

    Performance Analysis Done

--- Part_C.py
+++ Part_C.py
@@ -89,7 +89,7 @@ for i in range(0,len(Book_to_Mkt_C)):
         Book_to_Mkt_C.iloc[i,:] = np.array(B_M_3)  
 Book_to_Mkt_C[Book_to_Mkt_C == -99.99] = np.nan       
 ###############Q2
-lag = 12
+lag = 12 * 3
 
 
 Months = df_Returns.index
@@ -101,22 +101,21 @@ betas.columns = industries
 df_Daily_Returns[df_Daily_Returns == -99.99] = np.nan
 df_Daily_Excess_Returns = df_Daily_Returns.subtract(df_Fama['RF'], 0)
 time_start = time.clock()
-for i, d in enumerate(betas.index):
-    print(i, d)
+for d, i in betas.iterrows():
+    print(d)
     
     d_Last12 = d - dateutil.relativedelta.relativedelta(months=lag)
     X = df_Fama['Mkt-RF'][(df_Fama.index < d)&(df_Fama.index >= d_Last12)]
     X_np = np.array(X)
-    for j in range(0,len(industries)):
-        Y = np.array(df_Daily_Excess_Returns.iloc[:,j][(df_Daily_Excess_Returns.index < d)&(df_Daily_Excess_Returns.index >= d_Last12)])
+    X_np = sm.add_constant(X)
+    for j in industries:
+        Y = np.array(df_Daily_Excess_Returns.loc[:,j][(df_Daily_Excess_Returns.index < d)&(df_Daily_Excess_Returns.index >= d_Last12)])
         Y_np = np.array(Y)
-        betas.iloc[i,j] =  X_np.dot(Y_np) / (X_np.dot(X_np))
+        betas.loc[d,j] =  np.linalg.solve(np.dot(X_np.T, X_np), np.dot(X_np.T, Y_np))[1]
 time_elapsed = (time.clock() - time_start)
 ##Checking computation time difference between package and analatycal way
 
 beta_avg = betas.mean()
-
-
 
 
 
@@ -129,17 +128,18 @@ Idio_vol.index = df_Returns.index
 Idio_vol.columns = industries
 
 time_start = time.clock()
-for i, d in enumerate(Idio_vol.index): 
-    print(i, d)
+for d, i in Idio_vol.iterrows(): 
+    print(d)
     d_Last12 = d - dateutil.relativedelta.relativedelta(months=1)
-    X = df_Fama.iloc[:,:3][(df_Fama.index < d)&(df_Fama.index >= d_Last12)]
+    X = np.array(df_Fama.iloc[:,:3][(df_Fama.index < d)&(df_Fama.index >= d_Last12)])
     X = sm.add_constant(X)
-    for j in range(0,len(industries)):
-        Y = df_Daily_Excess_Returns.iloc[:,j][(df_Daily_Excess_Returns.index < d)&(df_Daily_Excess_Returns.index >= d_Last12)]
-        beta_Fama = np.linalg.solve(np.dot(np.array(X).T, np.array(X)), np.dot(np.array(X).T, np.array(Y)))
+    for j in industries:
+        Y = df_Daily_Excess_Returns.loc[:,j][(df_Daily_Excess_Returns.index < d)&(df_Daily_Excess_Returns.index >= d_Last12)]
+        Y = np.array(Y)
+        beta_Fama = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
         resid = Y -  X @ beta_Fama.T 
         std_resid = np.std(resid)
-        Idio_vol.iloc[i,j] = std_resid
+        Idio_vol.loc[d,j] = std_resid
 time_elapsed1 = (time.clock() - time_start)       
 # Checking time
 time_start = time.clock()
@@ -160,13 +160,13 @@ print(time_elapsed - time_elapsed1)
 
 # Parameters
 
-start_date = pd.to_datetime(datetime.datetime(1963, 1, 1))
+start_date = pd.to_datetime(datetime.datetime(1963, 7, 31))
 end_date = pd.to_datetime(datetime.datetime(2020, 1, 1))
 
 date_vec = df_Returns.index[(df_Returns.index >= start_date)&(df_Returns.index <= end_date)]
 
-num_long = 10
-num_short = 10
+num_long = 5
+num_short = 5
 
 # 1 : Long low characteristic and short high characteristic
 # -1 : Long high characteristic and short low characteristic
@@ -192,49 +192,61 @@ for s in strat:
     weight[s].columns = industries
 
 # Initialization
-returns = pd.Series(np.zeros(len(strat)))
-returns.index = strat
+returns = pd.DataFrame(np.zeros((len(date_vec), len(strat))))
+returns.index = date_vec
+returns.columns = strat
+
+returns_tmoins1 = pd.Series(np.zeros(len(strat)))
+returns_tmoins1.index = strat
 Nav_tmoins1 = pd.Series(np.ones(len(strat)))
 Nav_tmoins1.index = strat
+i = 0
 for date, Nav in NAV.iterrows():
     print(date, end_date)
-    NAV.loc[date,:] = np.array(Nav_tmoins1.T) * np.array(1+returns / 100)
+    i = i + 1 
+    NAV.loc[date,:] = np.array(Nav_tmoins1.T) * np.array(1 + returns_tmoins1 / 100)
     for characteristic_name in strat:       
-        
+        # investable universe
         is_excluded = pd.isna(df_Returns_tplus1.loc[date,:])
         is_eligible = np.logical_not(is_excluded)
+        
         # Low characteristic
-        idx_long = characteristic_tmp[characteristic_name].loc[date, is_eligible].nsmallest(num_long).index
+        idx_low = characteristic_tmp[characteristic_name].loc[date, is_eligible].nsmallest(num_long).index
         if weight_type == 'Equal':
-            weight[characteristic_name].loc[date, idx_long] = 1 / num_long * long_short.loc[characteristic_name]
+            weight[characteristic_name].loc[date, idx_low] = 1 / num_long * long_short.loc[characteristic_name]
         elif weight_type == 'Market_cap':
-            market_cap_long = characteristic_tmp['SMB'].loc[date, idx_long]
-            weight[characteristic_name].loc[date, idx_long] = market_cap_long \
+            market_cap_long = characteristic_tmp['SMB'].loc[date, idx_low]
+            weight[characteristic_name].loc[date, idx_low] = market_cap_long \
             / sum(market_cap_long) * long_short.loc[characteristic_name]
         elif weight_type == 'Rank':
-            long_rank = characteristic_tmp[characteristic_name].loc[date, idx_long].rank(ascending = False)
-            weight[characteristic_name].loc[date, long_rank.index] = long_rank \
-            /sum(long_rank) * long_short.loc[characteristic_name]
-        # high characteristic
-        idx_short = characteristic_tmp[characteristic_name].loc[date, is_eligible].nlargest(num_short).index
+            low_rank = characteristic_tmp[characteristic_name].loc[date, idx_low].rank(ascending = False)
+            weight[characteristic_name].loc[date, low_rank.index] = low_rank \
+            /sum(low_rank) * long_short.loc[characteristic_name]
+            
+        # High characteristic
+        idx_high = characteristic_tmp[characteristic_name].loc[date, is_eligible].nlargest(num_short).index
         if weight_type == 'Equal':
-            weight[characteristic_name].loc[date, idx_short] = -1 / num_short * long_short.loc[characteristic_name]
+            weight[characteristic_name].loc[date, idx_high] = - 1 / num_short * long_short.loc[characteristic_name]
         elif weight_type == 'Market_cap':
-            market_cap_short = characteristic_tmp['SMB'].loc[date, idx_short]
-            weight[characteristic_name].loc[date, idx_short] =  - market_cap_short \
+            market_cap_short = characteristic_tmp['SMB'].loc[date, idx_high]
+            weight[characteristic_name].loc[date, idx_high] =  - market_cap_short \
             / sum(market_cap_short) * long_short.loc[characteristic_name]
         elif weight_type == 'Rank':
-            short_rank = characteristic_tmp[characteristic_name].loc[date, idx_long].rank()
-            weight[characteristic_name].loc[date, short_rank.index] = short_rank \
-            / sum(short_rank) * long_short.loc[characteristic_name]
+            high_rank = characteristic_tmp[characteristic_name].loc[date, idx_high].rank()
+            weight[characteristic_name].loc[date, high_rank.index] = - high_rank \
+            / sum(high_rank) * long_short.loc[characteristic_name]
             
         # Returns
-        idx_weight = idx_long.append(idx_short)
-        returns.loc[characteristic_name] = weight[characteristic_name].loc[date, idx_weight].dot(df_Returns_tplus1.loc[date, idx_weight])
-
-        
-    Nav_tmoins1 = NAV.loc[date,:]
+        idx_weight = idx_low.append(idx_high)
+        returns.loc[date, characteristic_name] = weight[characteristic_name].loc[date, idx_weight].dot(df_Returns_tplus1.loc[date, idx_weight])
     
+    # Transfert for the next iteration
+    returns_tmoins1 = returns.loc[date, :]
+    Nav_tmoins1 = NAV.loc[date,:]
+
+
+
+  
 market = df_Fama_3.loc[:,'Mkt-RF'] + df_Fama_3.loc[:,'RF']
 NAV_market = np.cumprod(1 + market[market.index > '1963-01-31'] / 100)    
 for characteristic_name in strat:
@@ -242,15 +254,146 @@ for characteristic_name in strat:
 plt.plot(NAV_market, label = 'Market')
 plt.legend()
 plt.yscale('log')
+plt.title('NAV with weigth: ' + weight_type)
 plt.show()
 
 
+## Performance calculation
 
-plt.plot(NAV.loc[:,'SMB'], label = characteristic_name)
-plt.plot(100*np.cumproduct(1+df_Fama_3.loc[:,'SMB']/100))
-plt.yscale('log')
-plt.show()
 
+
+performance_measure_type = ['Sharpe_Ratio', 'Fama_3', 'Fama_4', 'Fama_5']
+nb_factor = [1, 4, 5, 6]
+factor = ['alpha', 'Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA']
+performance_measure = {}
+for s in strat:
+    performance_measure[s] = pd.DataFrame(np.zeros((len(factor), len(performance_measure_type))))
+    performance_measure[s].columns = performance_measure_type
+    performance_measure[s].index = factor
+
+for s in strat:
+    print(s)
+    for i, p in enumerate(performance_measure_type):
+        factor_tmp = factor[:nb_factor[i]]
+        if p == 'Sharpe_Ratio':
+            # Sharpe Ratio: Excess Return / Standard deviation
+            tmp = returns.loc[:,s].subtract(df_Fama_3.loc[date_vec, 'RF']).mean() / returns.loc[:,s].std()
+            
+        else: #Fama French
+            #Excess Return
+            Y = np.array(returns.loc[:,s].subtract(df_Fama_3.loc[date_vec, 'RF']))
+            if p == 'Fama_3':
+                X = df_Fama_3.loc[date_vec, factor_tmp[1:]]
+            else: # Fama_4 and Fama_5
+                X = df_Fama_5.loc[date_vec, factor_tmp[1:]]
+           
+            X = sm.add_constant(X)
+            tmp = np.linalg.solve(np.dot(np.array(X).T, np.array(X)), np.dot(np.array(X).T, np.array(Y)))
+
+        performance_measure[s].loc[factor_tmp,p] = tmp
+
+    print(performance_measure[s])
+
+
+
+
+# =============================================================================
+# #Regression on FAMA factors
+# #3-FACTORS
+# 
+# Periods = ['1949-12-01','1989-12-01','1999-12-01']
+# alpha_3_EW = pd.DataFrame(np.zeros((3,5)))
+# alpha_3_EW.columns = EW_Strat2_Returns.index
+# alpha_3_VW = pd.DataFrame(np.zeros((3,5)))
+# alpha_3_VW.columns = EW_Strat2_Returns.index
+# alpha_3_EW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# alpha_3_EW_LowStrat.columns = EW_Strat2_Returns.index
+# alpha_3_EW_Strat2 = pd.DataFrame(np.zeros((3,5)))
+# alpha_3_EW_Strat2.columns = EW_Strat2_Returns.index
+# alpha_3_VW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# alpha_3_VW_LowStrat.columns = EW_Strat2_Returns.index
+# 
+# Strategies_Returns = [Return_EW, Return_VW, Return_EW_LowStrat, Return_EW_Strat2, Return_VW_LowStrat]
+# alphas_3_Factors = [alpha_3_EW,alpha_3_VW,alpha_3_EW_LowStrat,alpha_3_EW_Strat2,alpha_3_VW_LowStrat]
+# 
+# for S in range(0, len(alphas_3_Factors)):
+#     for k in range(0,5):
+#         for i in range(0,len(Periods)):
+#             X = df_Fama_3.iloc[:,:3][(df_Fama_3.index < '2020-01-01')&(df_Fama_3.index > Periods[i])]/100
+#             X = sm.add_constant(X)
+#             Y = Strategies_Returns[S].iloc[:,k][(Strategies_Returns[S].index < '2020-01-01')&(Strategies_Returns[S].index > Periods[i])] - df_Fama_3.iloc[:,3][(df_Fama_3.index < '2020-01-01')&(df_Fama_3.index > Periods[i])]/100
+#             alpha = np.linalg.solve(np.dot(np.array(X).T, np.array(X)), np.dot(np.array(X).T, np.array(Y)))[0]
+#             alphas_3_Factors[S].iloc[i,k] = alpha
+# 
+# #4 & 5 Factors
+#         
+# Periods = ['1963-06-01','1989-12-01','1999-12-01']
+# alpha_4_EW = pd.DataFrame(np.zeros((3,5)))
+# alpha_4_EW.columns = EW_Strat2_Returns.index
+# alpha_4_VW = pd.DataFrame(np.zeros((3,5)))
+# alpha_4_VW.columns = EW_Strat2_Returns.index
+# alpha_4_EW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# alpha_4_EW_LowStrat.columns = EW_Strat2_Returns.index
+# alpha_4_EW_Strat2 = pd.DataFrame(np.zeros((3,5)))
+# alpha_4_EW_Strat2.columns = EW_Strat2_Returns.index
+# alpha_4_VW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# alpha_4_VW_LowStrat.columns = EW_Strat2_Returns.index
+# 
+# alphas_4_Factors = [alpha_4_EW,alpha_4_VW,alpha_4_EW_LowStrat,alpha_4_EW_Strat2,alpha_4_VW_LowStrat]
+# 
+# for S in range(0,len(alphas_4_Factors)):
+#     for k in range(0,5):
+#         for i in range(0,len(Periods)):
+#             X = df_Fama_5.iloc[:,:4][(df_Fama_5.index < '2020-01-01')&(df_Fama_5.index > Periods[i])]/100
+#             X = sm.add_constant(X)
+#             Y = Strategies_Returns[S].iloc[:,k][(Strategies_Returns[S].index < '2020-01-01')&(Strategies_Returns[S].index > Periods[i])] - df_Fama_5.iloc[:,4][(df_Fama_5.index < '2020-01-01')&(df_Fama_5.index > Periods[i])]/100
+#             alpha = np.linalg.solve(np.dot(np.array(X).T, np.array(X)), np.dot(np.array(X).T, np.array(Y)))[0]
+#             alphas_4_Factors[S].iloc[i,k] = alpha
+#         
+# alpha_5_EW = pd.DataFrame(np.zeros((3,5)))
+# alpha_5_EW.columns = EW_Strat2_Returns.index
+# alpha_5_VW = pd.DataFrame(np.zeros((3,5)))
+# alpha_5_VW.columns = EW_Strat2_Returns.index
+# alpha_5_EW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# alpha_5_EW_LowStrat.columns = EW_Strat2_Returns.index
+# alpha_5_EW_Strat2 = pd.DataFrame(np.zeros((3,5)))
+# alpha_5_EW_Strat2.columns = EW_Strat2_Returns.index
+# alpha_5_VW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# alpha_5_VW_LowStrat.columns = EW_Strat2_Returns.index
+# 
+# alphas_5_Factors = [alpha_5_EW,alpha_5_VW,alpha_5_EW_LowStrat,alpha_5_EW_Strat2,alpha_5_VW_LowStrat]
+# 
+# for S in range(0,len(alphas_5_Factors)):
+#     for k in range(0,5):
+#         for i in range(0,len(Periods)):
+#             X = df_Fama_5.iloc[:,:5][(df_Fama_5.index < '2020-01-01')&(df_Fama_5.index > Periods[i])]/100
+#             X = sm.add_constant(X)
+#             Y = Strategies_Returns[S].iloc[:,k][(Strategies_Returns[S].index < '2020-01-01')&(Strategies_Returns[S].index > Periods[i])] - df_Fama_5.iloc[:,4][(df_Fama_5.index < '2020-01-01')&(df_Fama_5.index > Periods[i])]/100
+#             alpha = np.linalg.solve(np.dot(np.array(X).T, np.array(X)), np.dot(np.array(X).T, np.array(Y)))[0]
+#             alphas_5_Factors[S].iloc[i,k] = alpha
+#         
+# #Sharpe
+# 
+# Periods = ['1949-12-01','1989-12-01','1999-12-01']
+# Sharpe_EW = pd.DataFrame(np.zeros((3,5)))
+# Sharpe_EW.columns = EW_Strat2_Returns.index
+# Sharpe_VW = pd.DataFrame(np.zeros((3,5)))
+# Sharpe_VW.columns = EW_Strat2_Returns.index
+# Sharpe_EW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# Sharpe_EW_LowStrat.columns = EW_Strat2_Returns.index
+# Sharpe_EW_Strat2 = pd.DataFrame(np.zeros((3,5)))
+# Sharpe_EW_Strat2.columns = EW_Strat2_Returns.index
+# Sharpe_VW_LowStrat = pd.DataFrame(np.zeros((3,5)))
+# Sharpe_VW_LowStrat.columns = EW_Strat2_Returns.index
+# 
+# Sharpe = [Sharpe_EW, Sharpe_VW, Sharpe_EW_LowStrat, Sharpe_EW_Strat2, Sharpe_VW_LowStrat]
+# 
+# for S in range(0, len(Strategies_Returns)):
+#     for k in range(0,5):
+#         for i in range(0,len(Periods)):
+#             Sharpe_i = (np.mean(Strategies_Returns[S].iloc[:,k][(Strategies_Returns[S].index < '2020-01-01')&(Strategies_Returns[S].index > Periods[i])] - df_Fama_3.iloc[:,3][(df_Fama_3.index < '2020-01-01')&(df_Fama_3.index > Periods[i])]/100))/np.std(Strategies_Returns[S].iloc[:,k][(Strategies_Returns[S].index < '2020-01-01')&(Strategies_Returns[S].index > Periods[i])])
+#             Sharpe[S].iloc[i,k] = Sharpe_i
+# =============================================================================
 # =============================================================================
 # Volatility_Q4 = Idio_vol[11:]
 # Book_to_Mkt_Q4 = Book_to_Mkt_C[11:]
